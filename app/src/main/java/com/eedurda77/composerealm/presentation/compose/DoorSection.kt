4767b9c06e7303eab6c55bd33d1caab95a9633e6
HayarovEd <edurd@rambler.ru>
2023-04-14 12:39:45 +0500

    complete design

--- app/src/main/java/com/eedurda77/composerealm/presentation/compose/DoorSection.kt
+++ app/src/main/java/com/eedurda77/composerealm/presentation/compose/DoorSection.kt
@@ -1,11 +1,9 @@
 package com.eedurda77.composerealm.presentation.compose
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Text
@@ -14,8 +12,10 @@ import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
@@ -38,7 +38,7 @@ fun DoorSection(
         modifier = modifier
             .fillMaxWidth()
             .background(colorResource(id = R.color.base_background))
-            .padding(11.dp)
+            .padding(horizontal = 21.dp, vertical = 15.dp)
             .pullRefresh(state = swipeRefreshState)
     ) {
         items(doors) { door ->
@@ -58,25 +58,51 @@ fun ItemDoor(
     onEvent: (MainEvent) -> Unit
 ) {
     Column(modifier = modifier
-        .padding(top = 10.dp)
+        .padding(top = 11.dp)
         .fillMaxWidth()
         .background(colorResource(id = R.color.white))
         .padding(11.dp)) {
-        Row(modifier = modifier
-            .fillMaxWidth()
-            .horizontalScroll(rememberScrollState())) {
-            Text(
-                modifier = modifier.padding(top = 22.dp, bottom = 20.dp, start = 16.dp),
-                color = colorResource(id = R.color.text_light_dark),
-                fontFamily = FontFamily(Font(R.font.circe)),
-                fontSize = 17.sp,
-                text = door.name ?: ""
+        Row(
+            modifier = modifier
+                .padding(vertical = 21.dp)
+                .fillMaxWidth()
+                .horizontalScroll(rememberScrollState())
+        ) {
+            Row(
+                modifier = modifier.width(333.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    modifier = modifier.padding(start = 16.dp),
+                    color = colorResource(id = R.color.text_light_dark),
+                    fontFamily = FontFamily(Font(R.font.circe)),
+                    fontSize = 17.sp,
+                    text = door.name ?: ""
+                )
+                Image(
+                    modifier = modifier
+                        .padding(end = 21.dp),
+                    imageVector = ImageVector.vectorResource(id = R.drawable.lockon),
+                    contentDescription = ""
+                )
+            }
+            Image(
+                modifier = modifier
+                    .padding(start = 33.dp),
+                imageVector = ImageVector.vectorResource(id = R.drawable.edit),
+                contentDescription = ""
+            )
+            Image(
+                modifier = modifier
+                    .padding(start = 9.dp, end = 21.dp),
+                imageVector = ImageVector.vectorResource(id = R.drawable.door_star),
+                contentDescription = ""
             )
         }
-        if (door.urlPath!=null) {
+        if (door.urlPath != null) {
             Box(
                 modifier = modifier
-                    .padding(top=8.dp)
+                    .padding(top = 8.dp)
                     .width(360.dp)
                     .height(207.dp)
                     .background(colorResource(id = R.color.white))
